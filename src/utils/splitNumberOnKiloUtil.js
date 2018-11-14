866ade9daf375fb8679df3b3e6087294f2884890
milosz <miloszhygen@gmail.com>
2018-11-14 22:18:32 +0100

    Rounded numbers down to have no decimals

--- src/utils/splitNumberOnKiloUtil.js
+++ src/utils/splitNumberOnKiloUtil.js
@@ -1,7 +1,7 @@
 
 export const splitNumberOnKiloUtil = (number) => {
   let splitNumber = ''
-  const splitNumberList = number.toString().match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
+  const splitNumberList = Math.round(number).toString().match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
   splitNumberList.map((item)=>{
     splitNumber += item + ' ';
   })
